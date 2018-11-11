3ee90d5e978560972f12f0c781aa8c044a723dbd
anavarreteuk <a.navarrete.uk@gmail.com>
2018-11-11 21:48:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 
+class Driver {
+  constructor(name, startDate) {
+    this.name = name
+    this.startDate = new Date (startDate)
+  }
+  yearsExperienceFromBeginningOf(year){
+
+    return new Date(year).getFullYear() - this.startDate.getFullYear()
+  }
+}
+
+
+
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+  blocksTravelled () {
+    let eastWest = [
+      '1st Avenue',
+      '2nd Avenue',
+      '3rd Avenue',
+      'Lexington Avenue',
+      'Park',
+      'Madison Avenue',
+      '5th Avenue'
+    ]
+
+    let verticalDistance = parseInt(
+      this.endingLocation.vertical - this.beginningLocation.vertical)
+
+    let horizontalDistance =
+      eastWest.indexOf(this.endingLocation.horizontal) -
+      eastWest.indexOf(this.beginningLocation.horizontal)
+      
+    return verticalDistance + horizontalDistance
+  }
+   estimatedTime (isPeakHour) {
+    let distance = this.blocksTravelled()
+    if (isPeakHour === true) {
+      return distance / 2
+    } else {
+      return distance / 3
+    }
+  }
+}
